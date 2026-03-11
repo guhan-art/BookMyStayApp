8c0a739601b447c80a4db5ea68aee2bc682c8138
guhan-art <guhanrajs2607@gmail.com>
2026-03-11 15:27:20 +0530

    UC5 base booking system

---
+++ BookMyStayApp.java
@@ -0,0 +1,118 @@
+import java.util.*;
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== Book My Stay System =====");
+
+        RoomInventory inventory = new RoomInventory();
+        BookingManager manager = new BookingManager(inventory);
+
+        manager.addBookingRequest("Rohan", "Single");
+        manager.addBookingRequest("Aryan", "Double");
+        manager.addBookingRequest("Kiran", "Suite");
+        manager.addBookingRequest("Rohan", "Single"); 
+
+        manager.processBookings();
+
+        inventory.displayInventory();
+    }
+}
+
+class RoomInventory {
+
+    private HashMap<String,Integer> availability;
+
+    public RoomInventory(){
+
+        availability = new HashMap<>();
+
+        availability.put("Single",2);
+        availability.put("Double",1);
+        availability.put("Suite",1);
+    }
+
+    public boolean bookRoom(String type){
+
+        if(availability.getOrDefault(type,0) > 0){
+
+            availability.put(type,availability.get(type)-1);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void displayInventory(){
+
+        System.out.println("\nRemaining Rooms:");
+
+        for(String type : availability.keySet()){
+
+            System.out.println(type + " : " + availability.get(type));
+        }
+    }
+}
+
+class BookingManager {
+
+    private Queue<BookingRequest> queue;
+    private RoomInventory inventory;
+    private Set<String> confirmedCustomers;
+
+    public BookingManager(RoomInventory inventory){
+
+        this.inventory = inventory;
+
+        queue = new LinkedList<>();
+        confirmedCustomers = new HashSet<>();
+    }
+
+    public void addBookingRequest(String name,String roomType){
+
+        queue.add(new BookingRequest(name,roomType));
+
+        System.out.println("Booking request added : " + name);
+    }
+
+    public void processBookings(){
+
+        System.out.println("\nProcessing bookings...");
+
+        while(!queue.isEmpty()){
+
+            BookingRequest request = queue.poll();
+
+            if(confirmedCustomers.contains(request.customerName)){
+
+                System.out.println("Duplicate booking rejected : " + request.customerName);
+                continue;
+            }
+
+            if(inventory.bookRoom(request.roomType)){
+
+                confirmedCustomers.add(request.customerName);
+
+                System.out.println("Booking confirmed : " + request.customerName);
+            }
+
+            else{
+
+                System.out.println("No rooms available for " + request.customerName);
+            }
+        }
+    }
+}
+
+class BookingRequest{
+
+    String customerName;
+    String roomType;
+
+    public BookingRequest(String name,String room){
+
+        this.customerName = name;
+        this.roomType = room;
+    }
+}
