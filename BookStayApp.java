26429ddc1506255346f6a13c9681c12b0445f8b7
guhan-art <guhanrajs2607@gmail.com>
2026-03-11 15:04:50 +0530

    UC3 - Centralized room inventory using HashMap

--- BookStayApp.java
+++ BookStayApp.java
@@ -1,49 +1,41 @@
+import java.util.HashMap;
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println(" Welcome to Book My Stay v1.0 ");
-        System.out.println(" Hotel Booking Management System ");
-        System.out.println("=================================");
+        System.out.println("===== Book My Stay - Room Inventory =====");
 
-        System.out.println("Application Started Successfully.");
-    }
-}
-abstract class Room {
+        RoomInventory inventory = new RoomInventory();
+        inventory.displayInventory();
 
-    protected String type;
-    protected double price;
-
-    public Room(String type, double price) {
-        this.type = type;
-        this.price = price;
-    }
-
-    public void displayDetails() {
-        System.out.println("Room Type: " + type);
-        System.out.println("Price: ₹" + price);
-    }
-}
-class SingleRoom extends Room {
-    public SingleRoom() {
-        super("Single Room", 2000);
+        System.out.println("\nChecking Availability of Single Room:");
+        System.out.println("Available: " + inventory.getAvailability("Single"));
     }
 }
 
-class DoubleRoom extends Room {
-    public DoubleRoom() {
-        super("Double Room", 3500);
+// ===== Room Inventory Class (UC3) =====
+class RoomInventory {
+
+    private HashMap<String, Integer> availability;
+
+    public RoomInventory() {
+        availability = new HashMap<>();
+
+        availability.put("Single", 5);
+        availability.put("Double", 3);
+        availability.put("Suite", 2);
+    }
+
+    public int getAvailability(String type) {
+        return availability.getOrDefault(type, 0);
+    }
+
+    public void displayInventory() {
+        System.out.println("Current Room Availability:");
+
+        for (String type : availability.keySet()) {
+            System.out.println(type + " Rooms: " + availability.get(type));
+        }
     }
 }
-
-class SuiteRoom extends Room {
-    public SuiteRoom() {
-        super("Suite Room", 6000);
-    }
-}
-int singleAvailable = 5;
-int doubleAvailable = 3;
-int suiteAvailable = 2;
-
-System.out.println("Available Single Rooms: " + singleAvailable);
